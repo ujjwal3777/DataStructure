b5ce80d2d35f62d9c9c17943675986309ba1303f
ujjwal.k <ujjwal.k@ezetap.com>
2024-03-02 18:05:14 +0530

    insertion Sort

--- DS/src/com/sorting/BubbleSort.java
+++ DS/src/com/sorting/BubbleSort.java
@@ -2,29 +2,25 @@ package com.sorting;
 
 import java.util.Arrays;
 
-public class BubbleSort {
+public class BubbleSort implements Sort{
     /* Bubble sort Compares adjacent element and later is less than previous its swap
      in this way it keeps on sorting elements from last on each iteration of an array
      bubble sort is a stable sorts meaning it preserves the order of sorting
      time Complexity - n*n
      space complexity : in place algorithm */
     public static void main (String ... args) {
-        int[] array = new int[7];
-        array[0] =12;
-        array[1] = 21;
-        array[2] = 7;
-        array[3] = 10;
-        array[4] = 9;
-        array[5] = 31;
-        array[6] = 2;
-        int temp =0;
+        Sort.initializeArray();
+        sort(array);
+
+    }
+
+
+    protected static void sort(int[] array) {
         int length =array.length-1;
         for(int i =0; i< length; i++) {
             for(int j=0; j < length -i ; j++) {
                 if(array[j] > array[j +1]) {
-                    temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] =temp;
+                    Sort.swap(array,j,j+1);
                 }
             }
         }
--- DS/src/com/sorting/SelectionSort.java
+++ DS/src/com/sorting/SelectionSort.java
@@ -2,22 +2,17 @@ package com.sorting;
 
 import java.util.Arrays;
 
-public class SelectionSort {
+public class SelectionSort implements Sort{
     /*Selection Sort finds maximum element in each iteration and swp the max element with last unsorted elements.
     Selection Sort is not stable sort
     Time Complexity : n*n
     Space Complexity : in place algorithm
     Doesn't require as much swap as Bubble Sort  */
     public static void main (String ... args) {
-        int[] array = new int[7];
-        array[0] =12;
-        array[1] = 21;
-        array[2] = 7;
-        array[3] = 10;
-        array[4] = 9;
-        array[5] = 31;
-        array[6] = 2;
-        int temp =0;
+        Sort.initializeArray();
+        sort(array);
+    }
+    protected static void sort(int[] array) {
         int length =array.length-1;
         for(int i =length; i>0; i--) {
             int max = 0;
@@ -26,11 +21,8 @@ public class SelectionSort {
                     max = j;
                 }
             }
-            temp = array[i];
-            array[i] = array[max];
-            array[max] = temp;
+            Sort.swap(array,i,max);
         }
-
         Arrays.stream(array).forEach(System.out :: println);
     }
 
